dbd988bbc7473c30a563e32ff09de135dc264532
Sanket F. <46294232+sanket-farande@users.noreply.github.com>
2025-12-11 11:26:58 +0000

    Learnt generics

--- src/main/java/_02_01/before/GenericsExample.java
+++ src/main/java/_02_01/before/GenericsExample.java
@@ -1,14 +1,29 @@
 package _02_01.before;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.List; 
+import java.awt.Rectangle;; 
 
 public class GenericsExample {
 
     public static void main(String[] args) {
 
-        List shapes = new ArrayList();
+        // Generics - Type for list elements, RHS infer from LHS so no need to mention
+        List <String> shapes = new ArrayList<>();
         shapes.add("Circle");
+
+        // typecasting is needed without generics
+        // String circle = (String) shapes.get(0);
+
+        String circle = shapes.get(0);
+        System.out.println(circle);
+
+        shapes.add("Rectangle");
+        System.out.println(shapes.get(1));
+
+        // This throws err with generics
+        // shapes.add(new Rectangle());
+        // Integer circle = (Integer) shapes.get(1);
 
     }
 
--- src/main/java/_02_02/before/GenericMethods.java
+++ src/main/java/_02_02/before/GenericMethods.java
@@ -6,12 +6,30 @@ import java.util.List;
 public class GenericMethods {
 
     public static void main(String[] args) {
-        String[] words = {"apple", "banana", "pear"};
-        Integer[] numbers = {1, 5, 7};
+        String[] words = { "apple", "banana", "pear" };
+        List<String> wordsList = convertToList(words);
+        Integer[] numbers = { 1, 5, 7 };
+        List<Integer> numberList = convertToList(numbers);
+        System.out.println(wordsList);
+        System.out.println(numberList);
+
+        Double[] doubles = { 1.3, 4.3, 2.0 };
+        List<Double> doubleList = convertNumberToList(doubles);
+        List<Integer> numberList2 = convertNumberToList(numbers);
+        System.out.println(doubleList);
+        System.out.println(numberList2);
     }
 
+    // Generic method
+    // T for Type. <T> Type declaration, List<T> Return type
+    public static <T> List<T> convertToList(T[] array) {
+        return Arrays.asList(array);
+    }
 
-
-
+    // Bounded Generic method: Allow only Numbers types
+    // T for Type. <T> Type declaration, List<T> Return type
+    public static <T extends Number> List<T> convertNumberToList(T[] array) {
+        return Arrays.asList(array);
+    }
 
 }
