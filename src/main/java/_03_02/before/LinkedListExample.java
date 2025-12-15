d23ac3cdc0817348483263372185ce9b85112efc
Sanket F. <46294232+sanket-farande@users.noreply.github.com>
2025-12-15 13:37:54 +0000

    Start LL and HM

--- src/main/java/_03_02/before/LinkedListExample.java
+++ src/main/java/_03_02/before/LinkedListExample.java
@@ -1,16 +1,23 @@
 package _03_02.before;
 
+import java.util.LinkedList;
+
 public class LinkedListExample {
 
     public static void main(String[] args) {
 
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("First");
+        linkedList.add("Second");
+        linkedList.add("Third");
+        System.out.println("LinkedList: " + linkedList);
+        
 
+        System.out.println(linkedList.getFirst());
+        System.out.println(linkedList.getLast());
 
+        System.out.println(linkedList.poll());
+        System.out.println(linkedList.pop()); //Throws exception if empty
     }
-
-
-
-
-
 
 }
--- src/main/java/_03_03/before/HashmapExample.java
+++ src/main/java/_03_03/before/HashmapExample.java
@@ -1,9 +1,23 @@
 package _03_03.before;
 
+import java.util.HashMap;
+
 public class HashmapExample {
 
     public static void main(String[] args) {
 
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("apple", 1);
+
+        // update value for existing key
+        map.put("apple", 3);
+
+        // set default 1 if not present, and increment by 1 if present
+        map.merge("apple",1,Integer::sum);
+        System.out.println(map.get("apple"));
+
+        map.merge("banana",1,Integer::sum);
+        System.out.println(map.get("banana"));
     }
 
 
